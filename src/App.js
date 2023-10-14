e5607a0edd382fb861fcff0e83cf8d49019210ee
Anna Zając <anza@netcompany.com>
2023-10-14 20:30:26 +0200

    remove edit line

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <img src="./staw.jpg" alt="staw" />
         <a
           className="App-link"
